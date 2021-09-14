993641bc983335f1e1ba75b41486723e320f4ade
Andrea Franz <andrea@gravityblast.com>
2021-09-14 18:31:02 +0200

    add missing commands

--- keycard_go/impl.nim
+++ keycard_go/impl.nim
@@ -5,3 +5,13 @@ proc Pair*(params: cstring): cstring {.importc: "Pair".}
 proc OpenSecureChannel*(params: cstring): cstring {.importc: "OpenSecureChannel".}
 proc VerifyPin*(params: cstring): cstring {.importc: "VerifyPin".}
 proc GenerateKey*(params: cstring): cstring {.importc: "GenerateKey".}
+proc DeriveKey*(params: cstring): cstring {.importc: "DeriveKey".}
+proc SignWithPath*(params: cstring): cstring {.importc: "SignWithPath".}
+proc ExportKey*(params: cstring): cstring {.importc: "ExportKey".}
+proc LoadSeed*(params: cstring): cstring {.importc: "LoadSeed".}
+proc Init*(params: cstring): cstring {.importc: "Init".}
+proc Unpair*(params: cstring): cstring {.importc: "Unpair".}
+proc GetStatusApplication*(): cstring {.importc: "GetStatusApplication".}
+proc ChangePin*(): cstring {.importc: "ChangePin".}
+proc ChangePuk*(): cstring {.importc: "ChangePuk".}
+proc ChangePairingPassword*(): cstring {.importc: "ChangePairingPassword".}
