bc645181d896b67f059c1d77964d20e6481ff4f1
Andrea Franz <andrea@gravityblast.com>
2021-09-27 14:16:50 +0200

    add select proc

--- keycard_go.nim
+++ keycard_go.nim
@@ -9,3 +9,9 @@ proc stop*(): string =
   var funcOut = go_shim.stop()
   defer: go_shim.free(funcOut)
   return $funcOut
+
+proc select*(): string =
+  var funcOut = go_shim.select()
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
