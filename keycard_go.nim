e959cec55edfc06ffe1567be36fdef63c761dae3
Andrea Franz <andrea@gravityblast.com>
2021-10-04 13:22:09 +0200

    add methods: opensecure channel, pair, verify pin, export key

--- keycard_go.nim
+++ keycard_go.nim
@@ -17,5 +17,25 @@ proc select*(): string =
   defer: go_shim.free(funcOut)
   return $funcOut
 
+proc pair*(params: cstring): string =
+  var funcOut = go_shim.pair(params)
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
+proc openSecureChannel*(params: cstring): string =
+  var funcOut = go_shim.openSecureChannel(params)
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
+proc verifyPin*(params: cstring): string =
+  var funcOut = go_shim.verifyPin(params)
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
+proc exportKey*(params: cstring): string =
+  var funcOut = go_shim.exportKey(params)
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
 proc setSignalEventCallback*(callback: KeycardSignalCallback) =
   go_shim.setSignalEventCallback(callback)
