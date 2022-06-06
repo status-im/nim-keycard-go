08d1b6e149fa334974793397fb3ec2fe3affbe9f
Sale Djenic <aleksandardjenic@status.im>
2022-06-06 12:11:41 +0200

    `keycard_go` updated to match changes done in the `status_keycard_go`

--- keycard_go/impl.nim
+++ keycard_go/impl.nim
@@ -1,22 +1,9 @@
 type KeycardSignalCallback* = proc(signal: cstring): void {.cdecl.}
 
-proc start*(): cstring {.importc: "Start".}
-proc stop*(): cstring {.importc: "Stop".}
-proc select*(): cstring {.importc: "Select".}
-proc pair*(params: cstring): cstring {.importc: "Pair".}
-proc openSecureChannel*(params: cstring): cstring {.importc: "OpenSecureChannel".}
-proc verifyPin*(params: cstring): cstring {.importc: "VerifyPin".}
-proc generateKey*(): cstring {.importc: "GenerateKey".}
-proc deriveKey*(params: cstring): cstring {.importc: "DeriveKey".}
-proc signWithPath*(params: cstring): cstring {.importc: "SignWithPath".}
-proc exportKey*(params: cstring): cstring {.importc: "ExportKey".}
-proc loadSeed*(params: cstring): cstring {.importc: "LoadSeed".}
-proc init*(params: cstring): cstring {.importc: "Init".}
-proc unpair*(params: cstring): cstring {.importc: "Unpair".}
-proc getStatusApplication*(): cstring {.importc: "GetStatusApplication".}
-proc changePin*(): cstring {.importc: "ChangePin".}
-proc changePuk*(): cstring {.importc: "ChangePuk".}
-proc changePairingPassword*(): cstring {.importc: "ChangePairingPassword".}
 proc free*(param: pointer) {.importc: "Free".}
 proc setSignalEventCallback*(callback: KeycardSignalCallback) {.importc: "KeycardSetSignalEventCallback".}
 
+proc keycardInitFlow*(storageDir: cstring): cstring {.importc: "KeycardInitFlow".}
+proc keycardStartFlow*(flowType: cint, jsonParams: cstring): cstring {.importc: "KeycardStartFlow".}
+proc keycardResumeFlow*(jsonParams: cstring): cstring {.importc: "KeycardResumeFlow".}
+proc keycardCancelFlow*(): cstring {.importc: "KeycardCancelFlow".}
