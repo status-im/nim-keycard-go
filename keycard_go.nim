a1724bad72e83ca83df2632d83a631cd8d8ff0f6
Sale Djenic <aleksandardjenic@status.im>
2023-09-13 17:17:05 +0200

    feat: new endpoints added to support keycard lib mocking

--- keycard_go.nim
+++ keycard_go.nim
@@ -24,3 +24,29 @@ proc keycardCancelFlow*(): string =
 
 proc setSignalEventCallback*(callback: KeycardSignalCallback) =
   go_shim.setSignalEventCallback(callback)
+
+# availale in test mode only
+proc mockedLibRegisterKeycard*(cardIndex: int, readerState: int, keycardState: int, mockedKeycard: string, mockedKeycardHelper: string): string =
+  var funcOut = go_shim.mockedLibRegisterKeycard(cardIndex.cint, readerState.cint, keycardState.cint, mockedKeycard.cstring, mockedKeycardHelper.cstring)
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
+proc mockedLibReaderPluggedIn*(): string =
+  var funcOut = go_shim.mockedLibReaderPluggedIn()
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
+proc mockedLibReaderUnplugged*(): string =
+  var funcOut = go_shim.mockedLibReaderUnplugged()
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
+proc mockedLibKeycardInserted*(cardIndex: int): string =
+  var funcOut = go_shim.mockedLibKeycardInserted(cardIndex.cint)
+  defer: go_shim.free(funcOut)
+  return $funcOut
+
+proc mockedLibKeycardRemoved*(): string =
+  var funcOut = go_shim.mockedLibKeycardRemoved()
+  defer: go_shim.free(funcOut)
+  return $funcOut
--- keycard_go/impl.nim
+++ keycard_go/impl.nim
@@ -7,3 +7,10 @@ proc keycardInitFlow*(storageDir: cstring): cstring {.importc: "KeycardInitFlow"
 proc keycardStartFlow*(flowType: cint, jsonParams: cstring): cstring {.importc: "KeycardStartFlow".}
 proc keycardResumeFlow*(jsonParams: cstring): cstring {.importc: "KeycardResumeFlow".}
 proc keycardCancelFlow*(): cstring {.importc: "KeycardCancelFlow".}
+
+# availale in test mode only
+proc mockedLibRegisterKeycard*(cardIndex: cint, readerState: cint, keycardState: cint, mockedKeycard: cstring, mockedKeycardHelper: cstring): cstring {.importc: "MockedLibRegisterKeycard".}
+proc mockedLibReaderPluggedIn*(): cstring {.importc: "MockedLibReaderPluggedIn".}
+proc mockedLibReaderUnplugged*(): cstring {.importc: "MockedLibReaderUnplugged".}
+proc mockedLibKeycardInserted*(cardIndex: cint): cstring {.importc: "MockedLibKeycardInserted".}
+proc mockedLibKeycardRemoved*(): cstring {.importc: "MockedLibKeycardRemoved".}
